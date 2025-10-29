c2af927af9ad9a3c7997ec2a41d4c53bff813534
nico-robert <nicolas.robert.19000@gmail.com>
2025-10-29 19:37:25 +0100

    Adds TCL_SIZE_MAX.

--- src/png.tcl
+++ src/png.tcl
@@ -184,6 +184,17 @@ if {
         #include "stb_image_write.h"
         #include <stdlib.h>
         #include <math.h>
+
+        #if TCL_MAJOR_VERSION == 8
+            #ifndef TCL_SIZE_MAX
+                typedef int Tcl_Size;
+                # define Tcl_GetSizeIntFromObj Tcl_GetIntFromObj
+                # define Tcl_NewSizeIntObj Tcl_NewIntObj
+                # define TCL_SIZE_MAX      INT_MAX
+                # define TCL_SIZE_MODIFIER ""
+            #endif
+        #endif
+
     }
 
     rename tresvg::encodePNG ""
